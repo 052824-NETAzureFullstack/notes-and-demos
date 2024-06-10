e7c931f87766817d123a2f5693d7222e648776ad
Richard Hawkins <Richard.Hawkins@revature.com>
2024-06-10 13:03:37 -0400

    finish up CoinFlipp game logic

--- NET/myFirstConsole/Program.cs
+++ NET/myFirstConsole/Program.cs
@@ -2,32 +2,69 @@
 {
     public static void Main(string[] args)
     {
-        // inline comments are created with the "//"
-        /* or block comments can start and end with  the "/*" and */
+        // // inline comments are created with the "//"
+        // /* or block comments can start and end with  the "/*" and */
 
-        // console refers to the terminal console, or the console window that .NET will create.
-        // we have .ReadLine and .WriteLine methods
+        // // console refers to the terminal console, or the console window that .NET will create.
+        // // we have .ReadLine and .WriteLine methods
 
-        Console.WriteLine("Please enter your name: ");
+        // Console.WriteLine("Please enter your name: ");
 
-        // when creating a variable, declare the type/collection!
-        string name;
-        name = Console.ReadLine();
+        // // when creating a variable, declare the type/collection!
+        // string name;
+        // name = Console.ReadLine();
 
-        // we can leverage concatenation to "add" strings together on the fly (works with string variables too!)
-        Console.WriteLine("Hello, " + name + "!");
+        // // we can leverage concatenation to "add" strings together on the fly (works with string variables too!)
+        // Console.WriteLine("Hello, " + name + "!");
         
         // tell the code to run the CoinFlip method
-        CoinFlip();
+
+        // while loop - let us keep repeating until the condition is false
+        // do while - do the action, then check the condition. loop if true
+        // for loop - defined starting, conditon, and step values
+        // foreach - enhanced for loop - iterates through an IEnumerable
+
+        bool cont = false;
+        do
+        {
+           cont = CoinFlip();
+           Console.Clear();
+        } while(cont);
+        
+        Console.WriteLine("Thanks for playing!");
 
         return;
     }
 
-    public static void CoinFlip()
+    public static bool CoinFlip()
     {
-        Console.WriteLine("CoinFlip starting...");
+        // True == Tails (keep the t's together!)
 
-        // boolean variables are noted with "bool"
+        Console.WriteLine("CoinFlip starting...");
+        bool coin = NewCoin();
+        bool user = PlayerGuess();
+
+
+       
+        // we could swap the coin variable type to a string
+        // we could add another string variable to add "heads" or "tails"
+        // we could have a collection and use the coin value as a key to "heads" and "tails" values - Collections/ Dictionary
+        // we could use an inline ternary statemnt to select the right side of the coin based on the the bool
+
+        Console.WriteLine("The coin was flipped, and landed on: " + ((coin) ? "tails" : "heads") );
+        Console.WriteLine("You chose: " + ((coin) ? "tails" : "heads") );
+        Console.WriteLine((coin == user) ? "Congratulations, you guessed correctly!" : "Sorry, you guessed wrong!");
+
+
+        Console.WriteLine("Play again? [y/n]");
+        string playAgain = Console.ReadLine();
+
+        return (playAgain == "y" || playAgain == "Y" || playAgain == "yes" || playAgain == "yup");
+    }
+
+    public static bool NewCoin()
+    {
+         // boolean variables are noted with "bool"
         bool coin; 
 
         // we can use a pseudo-random number generator to "flip" our coin
@@ -45,11 +82,33 @@
             coin = true;
         }
 
-        // we could swap the coin variable type to a string
-        // we could add another string variable to add "heads" or "tails"
-        // we could have a collection and use the coin value as a key to "heads" and "tails" values - Collections/ Dictionary
-        // we could use an inline ternary statemnt to select the right side of the coin based on the the bool
+        return coin;
+    }
 
-        Console.WriteLine("The coin was flipped, and landed on: " + ((coin) ? "tails" : "heads") );
+    public static bool PlayerGuess()
+    {
+        // prompt the player to select heads or tails, and return true or false
+        // accept a string for "heads" or "tails"
+        // accept a numerical representation (1 or 0)
+
+        Console.WriteLine("Please select Heads or Tails:");
+        Console.WriteLine("[0] - Heads");
+        Console.WriteLine("[1] - Tails");
+
+        // With Console.ReadLine, everyting that is entered is a string. we need to convert if we want something else.
+        //string userInput = Console.ReadLine();
+
+        int userChoice = Int32.Parse(Console.ReadLine());
+
+        if (userChoice == 1 || userChoice == 0) // only a 1 or a 0 will enter the if statement
+        {
+            return (userChoice == 1)? true : false;
+        }
+        else
+        {
+            Console.WriteLine("Improper input detected");
+        }
+
+        return false;
     }
 }
