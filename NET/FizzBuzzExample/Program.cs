64887dc2467b2198ff4fa554ee669471441c21f6
Richard Hawkins <Richard.Hawkins@revature.com>
2024-06-11 10:22:03 -0400

    added user input for uppper and lower values - FizzBuzz

--- NET/FizzBuzzExample/Program.cs
+++ NET/FizzBuzzExample/Program.cs
@@ -6,8 +6,18 @@ namespace FizzBuzz
     {
         public static void Main(string[] args)
         {
+            // Prompt the user to input the lower and upper values to play with
+
+            Console.Write("Please enter a valid lower limit: ");
+            string userIn = Console.ReadLine();
+            int lower = Int32.Parse(userIn);
+
+            Console.Write("Please enter a valid upper limit: ");
+            userIn = Console.ReadLine();
+            int upper = Int32.Parse(userIn);
+
             // a numbered loop, printing out the number in question on a new line.
-            for ( int i = 0; i < 10; i++)
+            for ( int i = lower; i <= upper; i++)
             {
                 Console.WriteLine(i);
             }
