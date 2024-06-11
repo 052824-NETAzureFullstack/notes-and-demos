f12150b4b567ec4002dda2ed752701e32b6dd1da
Richard Hawkins <Richard.Hawkins@revature.com>
2024-06-11 10:16:02 -0400

    intial phase - FizzBuzz

---
+++ NET/FizzBuzzExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // a numbered loop, printing out the number in question on a new line.
+            for ( int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
