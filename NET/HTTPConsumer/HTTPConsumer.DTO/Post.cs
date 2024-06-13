a51972e94c660c105ac5164f0608966e9a47eea6
Richard Hawkins <Richard.Hawkins@revature.com>
2024-06-13 11:17:01 -0400

    finalize HTTPConsumer app

--- NET/HTTPConsumer/HTTPConsumer.DTO/Post.cs
+++ NET/HTTPConsumer/HTTPConsumer.DTO/Post.cs
@@ -1,11 +1,12 @@
-﻿namespace HTTPConsumer.DTO;
-
-public class Post
+﻿namespace HTTPConsumer.DTO
 {
-    // Fields
-    // DTO - Data Transfer Object
-    public int userId { get; set; }
-    public int id { get; set; }
-    public string? title { get; set; }
-    public string? body { get; set; }
+    public class Post
+    {
+        // Fields
+        // DTO - Data Transfer Object
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string? title { get; set; }
+        public string? body { get; set; }
+    }
 }
