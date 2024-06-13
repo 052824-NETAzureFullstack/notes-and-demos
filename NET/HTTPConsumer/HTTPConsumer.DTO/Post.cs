01068ddd72ca5864f2a9ee8bc231c202d7274ec0
Richard Hawkins <Richard.Hawkins@revature.com>
2024-06-13 11:00:46 -0400

    added HttpConsumer app and DTO

---
+++ NET/HTTPConsumer/HTTPConsumer.DTO/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace HTTPConsumer.DTO;
+
+public class Post
+{
+    // Fields
+    // DTO - Data Transfer Object
+    public int userId { get; set; }
+    public int id { get; set; }
+    public string? title { get; set; }
+    public string? body { get; set; }
+}
